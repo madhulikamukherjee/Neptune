b27b552011a9ea0a45da893c47a15c326f151fee
Madhulika Mukherjee <madhulikamukherjee.15@gmail.com>
2016-09-05 10:33:15 +0530

    separated controllers in javascript.

--- neptune_app/static/js/main.js
+++ neptune_app/static/js/main.js
@@ -1,25 +1,8 @@
 var app=angular.module('NeptuneApp',['ngRoute']);
 
-app.controller("PlanetsController", [ '$scope', '$http', '$routeParams', function($scope,$http,$routeParams){
-  
-  var planet_id = $routeParams.planet_id;
-  uri = '/planets/api/'+planet_id;
 
-  $http.get(uri).success(function(data){
-    $scope.returned_string = "Planet name is: "+ data.name + " with radius: " + data.radius;
-  });
 
-}]);
 
-app.controller("MoonsController", [ '$scope', '$http', '$routeParams', function($scope,$http,$routeParams){
-  
-  var moon_id = $routeParams.moon_id;
-  uri = '/moons/api/'+ moon_id;
-  $http.get(uri).success(function(data){
-    $scope.returned_string = "Moon is: "+ data.name + " which was discovered by: " + data.discoverer;
-  });
-  
-}]);
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
    $locationProvider.html5Mode({
